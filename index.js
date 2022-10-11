fe961f275ecf2a9227c6c686f11b9887a2f2427e
prabhat7k <re311001boot@gmail.com>
2022-10-12 00:16:45 +0530

    Added the dark mode

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+function darkMode() {
+  var element = document.body;
+  var content = document.getElementById("DarkModetext");
+  element.className = "dark-mode";
+  content.innerText = "Dark Mode is ON";
+  element.className = "arrow";
+}
+  function lightMode() {
+  var element = document.body;
+  var content = document.getElementById("DarkModetext");
+  element.className = "light-mode";
+  content.innerText = "Dark Mode is OFF";
+}
 const setNormalView = isNormalView => {
     if (isNormalView) {
       document.querySelector('#normal-view').classList.add('selected');
