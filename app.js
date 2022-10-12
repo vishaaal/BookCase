d9d24554c6d87b8d68564547556a40abd3c64e7c
Parth Lunagariya <88667075+Pruthvik1@users.noreply.github.com>
2022-10-12 10:15:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("cart-info").addEventListener("click", function() {
+  const cart = document.getElementById("cart");
+  cart.classList.toggle("show-cart");
+  console.log(cart);
+});
