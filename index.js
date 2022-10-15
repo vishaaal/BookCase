931e51f5be118dac90a02630e6d7522dfcc9fbac
Vandana <vp317470@gmail.com>
2022-10-16 00:05:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 
 function darkMode() {
-  var element = document.body;
-  var content = document.getElementById("DarkModetext");
-  element.className = "dark-mode";
-  content.innerText = "Dark Mode is ON";
-  element.className = "arrow";
+jQuery('body').toggleClass('day');
+    jQuery('body').toggleClass('night');
 }
-  function lightMode() {
-  var element = document.body;
-  var content = document.getElementById("DarkModetext");
-  element.className = "light-mode";
-  content.innerText = "Dark Mode is OFF";
-}
+
 const setNormalView = isNormalView => {
     if (isNormalView) {
       document.querySelector('#normal-view').classList.add('selected');
